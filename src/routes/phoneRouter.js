85600769dc7bb56b421448d483706e3e963abf46
Sergio Radigales <sergioradigales@gmail.com>
2022-03-07 18:56:06 +0100

    fix2

--- src/routes/phoneRouter.js
+++ src/routes/phoneRouter.js
@@ -5,6 +5,8 @@ router.get('/', async (req,res,next) => {
     const phones = await phoneService.readAll()
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     res.json(phones)
 })
